0074ed55cd5cc9715484f1417e8fe65b639132c8
Parth Pandey <parthpandeyindian@gmail.com>
2025-08-23 22:56:07 +0530

    Add files via upload

---
+++ operations.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    float num1, num2;   
+    float sum, difference, product, quotient;
+
+    // Input two numbers
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+    
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    // Perform operations
+    sum = num1 + num2;
+    difference = num1 - num2;
+    product = num1 * num2;
+
+    // Check for division by zero
+    if (num2 != 0) {
+        quotient = num1 / num2;
+        printf("\nQuotient: %.2f", quotient);
+    } else {
+        printf("\nDivision by zero is not allowed.");
+    }
+
+    printf("\nSum: %.2f", sum);
+    printf("\nDifference: %.2f", difference);
+    printf("\nProduct: %.2f", product);
+    
+
+    return 0;
+}
---
+++ sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, sum;
+     printf("Enter a number:\n");
+     scanf("%d",&a);
+     printf("Enter another number:");
+     scanf("%d",&b);
+     sum=a+b;
+ printf("Sum: %d",sum);
+ return 0;
+}
