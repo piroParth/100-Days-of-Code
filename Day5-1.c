42d5b250a0e8bf9428735a7892973ae74ca20c04
Parth Pandey <parthpandeyindian@gmail.com>
2025-08-25 23:38:13 +0530

    Update Day5-1.c

--- Day5-1.c
+++ Day5-1.c
@@ -1,3 +1,4 @@
+//Write a program to calculate simple and compound interest for given principal, rate, and time.
 #include <stdio.h>
 #include <math.h>  // for pow() function
 
@@ -27,3 +28,4 @@ int main() {
 
     return 0;
 }
+
