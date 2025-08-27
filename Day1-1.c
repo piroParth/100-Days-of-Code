fb5091df3d4cc73248f2bf4c959e2c913f140c80
Parth Pandey <parthpandeyindian@gmail.com>
2025-08-27 11:57:18 +0530

    Update Day1-1.c

--- Day1-1.c
+++ Day1-1.c
@@ -7,20 +7,14 @@
 int main() {
     float num1, num2;   
     float sum, difference, product, quotient;
-
-    // Input two numbers
     printf("Enter first number: ");
     scanf("%f", &num1);
     
     printf("Enter second number: ");
     scanf("%f", &num2);
-
-    // Perform operations
     sum = num1 + num2;
     difference = num1 - num2;
     product = num1 * num2;
-
-    // Check for division by zero
     if (num2 != 0) {
         quotient = num1 / num2;
         printf("\nQuotient: %.2f", quotient);
@@ -35,4 +29,5 @@ int main() {
 
     return 0;
 }
+
 
