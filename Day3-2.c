896e1b3c219345fad88e6b07e944dc207a40c09b
Parth Pandey <parthpandeyindian@gmail.com>
2025-08-27 11:55:59 +0530

    Update Day3-2.c

--- Day3-2.c
+++ Day3-2.c
@@ -3,25 +3,20 @@
 
 int main() {
     int a, b, temp;
-
-    // Input two numbers
     printf("Enter first number: ");
     scanf("%d", &a);
 
     printf("Enter second number: ");
     scanf("%d", &b);
-
-    // Swap using a third variable
     temp = a;
     a = b;
     b = temp;
-
-    // Output after swapping
     printf("After swapping:\n");
     printf("First number = %d\n", a);
     printf("Second number = %d\n", b);
 
     return 0;
 }
+
 
 
