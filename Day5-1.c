275d8a0522a8e6c7ca2a9653b79174a30ed03503
Parth Pandey <parthpandeyindian@gmail.com>
2025-08-27 12:04:51 +0530

    Update Day5-1.c

--- Day5-1.c
+++ Day5-1.c
@@ -1,11 +1,9 @@
-//Write a program to calculate simple and compound interest for given principal, rate, and time.
+//Write a program to calculate simple and compound interest for a given principal, rate, and time.
 #include <stdio.h>
 #include <math.h>  // for pow() function
 
 int main() {
     float principal, rate, time, simpleInterest, compoundInterest, amount;
-
-    // Input values
     printf("Enter Principal amount: ");
     scanf("%f", &principal);
 
@@ -21,11 +19,11 @@ int main() {
     // Calculate Compound Interest
     amount = principal * pow((1 + rate / 100), time);
     compoundInterest = amount - principal;
-
-    // Output results
+    
     printf("\nSimple Interest = %.2f", simpleInterest);
     printf("\nCompound Interest = %.2f", compoundInterest);
 
     return 0;
 }
+
 
