91c0d9949574be5ba527d130956fc305dd528712
Parth Pandey <parthpandeyindian@gmail.com>
2025-09-07 19:29:51 +0530

    Add files via upload

---
+++ Day18-2.c
@@ -0,0 +1,17 @@
+//Write a program to find the HCF (GCD) of two numbers.
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("HCF = %d\n", a);
+    return 0;
+}
