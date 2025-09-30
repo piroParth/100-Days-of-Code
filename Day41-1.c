2d68cc2606119b77ae5ac69c4622c65ae5861736
Parth Pandey <parthpandeyindian@gmail.com>
2025-09-30 20:17:41 +0530

    Add files via upload

---
+++ Day41-1.c
@@ -0,0 +1,12 @@
+//Count characters in a string without using built-in length functions.
+#include <stdio.h>
+int main() {
+    char str[1000];
+    int count = 0;
+    fgets(str, sizeof(str), stdin);
+    while (str[count] != '\0' && str[count] != '\n') {
+        count++;
+    }
+    printf("%d\n", count);
+    return 0;
+}
