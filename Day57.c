961bc5aaec074a333daebc28d7631fdc457522c8
Parth Pandey <parthpandeyindian@gmail.com>
2025-10-29 19:18:36 +0530

    Update Day57.c

--- Day57.c
+++ Day57.c
@@ -1,3 +1,4 @@
+//Q107: Write a program to take an array arr[] of integers as input, the task is to find the previous greater element for each element of the array in order of their appearance in the array. Previous greater element of an element in the array is the nearest element on the left which is greater than the current element. If there does not exist next greater of current element, then previous greater element for current element is -1.
 #include <stdio.h>
 int main() {
     int n;
